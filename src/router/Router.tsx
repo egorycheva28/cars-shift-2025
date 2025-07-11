2785afdb98521cf8e84bcc9b6514896636f76012
egorycheva28 <egor.masha28@list.ru>
2025-07-11 17:48:11 +0700

    add login page

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,6 +5,7 @@ import CarsListPage from '../pages/Cars/CarsListPage';
 import CarByIdPage from '../pages/Cars/CarByIdPage';
 import CarRentalPage from '../pages/CarRental/CarRentalPage';
 import CarRentPage from '../pages/CarRental/CarRentPage';
+import AuthPage from '../pages/Auth/AuthPage';
 
 const Router: React.FC = () => {
 
@@ -17,6 +18,7 @@ const Router: React.FC = () => {
                 <Route path="/car/:id" element={<CarByIdPage />} />
                 <Route path="/car/rent/:id" element={<CarRentalPage />} />
                 <Route path="/car/rent" element={<CarRentPage />} />
+                <Route path="/login" element={<AuthPage />} />
             </Routes>
         </>
     );
